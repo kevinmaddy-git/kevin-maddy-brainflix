0052aa4d8961c48e4f53494333ff06fddf83f2e9
kevinmaddy-git <kevbenefitsyou@gmail.com>
2023-05-16 15:26:16 -0400

    Hero Video added and styled with generic controls player

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import "./App.scss";
 import Navbar from "./components/Component/Navbar";
+import HeroVideo from "./components/Component/HeroVideo"
+import CurrentVideoDetails from './data/video-details.json';
+import VideoData from './data/videos.json'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-      </div>
-    );
+function App() {
+  const[videoData, setVideoData] = useState(VideoData)
+  const [currentVideo, setCurrentVideo] = useState(CurrentVideoDetails[0])
+
+  const switchVideo = (id) => {
+    const selectedVideo = CurrentVideoDetails.find((video) => {
+      return video.id === id;
+    })
+    setCurrentVideo(selectedVideo);
   }
+  return (
+    <div className="App">
+      <Navbar />
+      <HeroVideo currentVideo={currentVideo} />
+    </div>
+  );
 }
 
 export default App;
