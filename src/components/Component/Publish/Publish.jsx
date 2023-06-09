0ba4ef2bca958bcf1907e13490c5b0b018080b8a
kevinmaddy-git <kevbenefitsyou@gmail.com>
2023-06-08 21:26:28 -0400

    Added a Link to Cancel button to return to Home Page

--- src/components/Component/Publish/Publish.jsx
+++ src/components/Component/Publish/Publish.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import uploadIcon from '../../../assets/images/Icons/publish.svg';
 import '../Publish/Publish.scss';
 
-function Publish({ history }) {
-  const handleCancel = () => {
-    history.push('/home');
-  };
-
+function Publish() {
   return (
     <div className='publish'>
       <button className="publish__button">
         <img className="publish__button-icon" src={uploadIcon} alt="Upload Icon" />
         PUBLISH
       </button>
-      <button className="cancel__button" onClick={handleCancel}>
+      <Link to="/" className="cancel__button">
         CANCEL
-      </button>
+      </Link>
     </div>
   );
 }
 
 export default Publish;
+
