9dc5f34c3eb938b60de8abfa2b701c202e642b5d
kevinmaddy-git <kevbenefitsyou@gmail.com>
2023-06-17 10:20:49 -0400

    Added alert for upload

--- src/components/Component/Publish/Publish.jsx
+++ src/components/Component/Publish/Publish.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import uploadIcon from '../../../assets/images/Icons/publish.svg';
 import '../Publish/Publish.scss';
 
 function Publish() {
+  const navigate = useNavigate();
+
+  const handleFormSubmit = () => {
+    alert('Upload successful!');
+    navigate('/');
+  };
+
   return (
     <div className='publish'>
-      <button className="publish__button">
+      <button className="publish__button" onClick={handleFormSubmit}>
         <img className="publish__button-icon" src={uploadIcon} alt="Upload Icon" />
         PUBLISH
       </button>
